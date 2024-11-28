8e01b9841308dfe0d6d2beef6d8df567e1ff9d9f
Ryan19900529 <ab00999819@gmail.com>
2024-11-28 14:14:27 -0800

    redundant code clean up

--- src/components/RenderItem.js
+++ src/components/RenderItem.js
@@ -1,22 +1,9 @@
 import React from "react";
-// import { useSortable } from "@dnd-kit/sortable";
-// import { CSS } from "@dnd-kit/utilities";
 
 const RenderItem = ({ renderItem, isDragging, renderRefs }) => {
-  // const { attributes, listeners, setNodeRef, transform, transition } =
-  //   useSortable({ id: renderItem.id });
-
-  // const style = {
-  //   transform: CSS.Transform.toString(transform),
-  //   transition,
-  // };
-
   return (
     <div
       ref={(el) => (renderRefs.current[renderItem.id] = el)}
-      // {...attributes}
-      // {...listeners}
-      // style={style}
       id={renderItem.id}
       className={`render_box_img_wrapper ${
         renderItem.visibility ? "" : "hide"
